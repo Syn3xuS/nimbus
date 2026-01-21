6e15f71abdabd83cb2931d09161636c178b25f4a
Syn3xuS <upiraliev@gmail.com>
2026-01-21 16:46:18 +0400

    Новые странички

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
-	return <>dsfisjfioo</>;
+export default function page() {
+	return <>Главная страница</>;
 }
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,17 +1,21 @@
-import "./style.css";
+import "./styles.css";
 import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/vercel.svg";
 
 const Header = () => {
 	return (
 		<header>
 			<div className="brand-banner">
-				<Image
-					width={100}
-					height={100}
-					src="https://upload.wikimedia.org/wikipedia/en/9/90/The_DuckDuckGo_Duck.png"
-					alt=""
-					className="brand-banner_logo"
-				/>
+				<Link href="/">
+					<Image
+						width={100}
+						height={100}
+						src={logo}
+						alt=""
+						className="brand-banner_logo"
+					/>
+				</Link>
 				<span className="brand-banner_name"></span>
 			</div>
 		</header>
